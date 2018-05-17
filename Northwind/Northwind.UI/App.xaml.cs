1394dec05857009a1bff5c10d870da183ef835a6
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-17 17:46:04 +0900

    Modified the folder structure of this UI project.

--- Northwind/Northwind.UI/App.xaml.cs
+++ Northwind/Northwind.UI/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Northwind.UI
 {
@@ -13,5 +7,6 @@ namespace Northwind.UI
     /// </summary>
     public partial class App : Application
     {
+        //TODO
     }
 }
