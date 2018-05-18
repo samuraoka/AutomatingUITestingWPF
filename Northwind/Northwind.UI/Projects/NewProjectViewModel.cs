74781751c9a70e7dfb2e7151c0ed58f95fb56669
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-19 00:02:46 +0900

    Implemented some code to display the New project dialog.

--- Northwind/Northwind.UI/Projects/NewProjectViewModel.cs
+++ Northwind/Northwind.UI/Projects/NewProjectViewModel.cs
@@ -5,5 +5,24 @@ namespace Northwind.UI.Projects
     public class NewProjectViewModel : ViewModel
     {
         //TODO
+        public ProjectMainPropertiesViewModel MainProperties { get; private set; }
+        //TODO
+
+        public override string Caption
+        {
+            get { return "New project"; }
+        }
+
+        public override double Width
+        {
+            get { return 400; }
+        }
+
+        public override double Height
+        {
+            get { return 238; }
+        }
+
+        //TODO
     }
 }
---
+++ Northwind/Northwind.UI/Projects/ProjectMainPropertiesViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Northwind.UI.Projects
+{
+    public class ProjectMainPropertiesViewModel
+    {
+    }
+}
