03b9ad52638cb47616fba7c674fe76af19495bb9
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-05-19 20:43:50 +0900

    Implemented some code to bind data and actions to the Employee List view.

---
+++ Northwind/Northwind.Logic/Model/EmployeeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Northwind.Logic.Model
+{
+    public class EmployeeDto
+    {
+        //TODO
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string DepartmentName { get; private set; }
+
+        //TODO
+    }
+}
--- Northwind/Northwind.UI/Employees/EmployeeListViewModel.cs
+++ Northwind/Northwind.UI/Employees/EmployeeListViewModel.cs
@@ -1,16 +1,52 @@
-﻿using Northwind.UI.Common;
+﻿using Northwind.Logic.Model;
+using Northwind.UI.Common;
+using System;
+using System.Collections.ObjectModel;
 
 namespace Northwind.UI.Employees
 {
     public class EmployeeListViewModel : ViewModel
     {
         //TODO
+        public Command AddEmployeeCommand { get; private set; }
+        public Command<EmployeeDto> EditEmployeeCommand { get; private set; }
+        public Command<EmployeeDto> DeleteEmployeeCommand { get; private set; }
+        public ObservableCollection<EmployeeDto> Employees { get; private set; }
 
         public override string Caption
         {
             get { return "Employees"; }
         }
 
+        public EmployeeListViewModel()
+        {
+            //TODO
+
+            AddEmployeeCommand = new Command(AddEmployee);
+            EditEmployeeCommand =
+                new Command<EmployeeDto>(x => x != null, EditEmployee);
+            DeleteEmployeeCommand =
+                new Command<EmployeeDto>(x => x != null, DeleteEmployee);
+        }
+
         //TODO
+
+        private void AddEmployee()
+        {
+            //TODO next
+            throw new NotImplementedException();
+        }
+
+        private void EditEmployee(EmployeeDto employeeDto)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        private void DeleteEmployee(EmployeeDto obj)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
     }
 }
