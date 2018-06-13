8ab25c44ea5d8e08635db967cd920be2a879b9e6
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-06-14 03:32:30 +0900

    Implemented some code but dosen't work.

--- Northwind/Northwind.White/Properties/AssemblyInfo.cs
+++ Northwind/Northwind.White/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Northwind.White")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2c31e07f-624a-475d-82fc-ff9dc11a2332")]
+[assembly: Guid("d510dc1e-7d0e-4d73-b828-9e1a8230d0d2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Northwind/Northwind.White/Tests.cs
+++ Northwind/Northwind.White/Tests.cs
@@ -1,13 +1,59 @@
-﻿namespace Northwind.White
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.Utility;
+
+namespace Northwind.White
 {
-    // xunit 2.3.1
-    // https://www.nuget.org/packages/xunit/2.3.1
-    // Install-Package xunit -Version 2.3.1 -ProjectName Northwind.White
-    //
-    // TestStack.White 0.13.3
-    // https://www.nuget.org/packages/TestStack.White/
-    // Install-Package TestStack.White -Version 0.13.3 Northwind.White
+    [TestClass]
     public class Tests
     {
+        [TestMethod]
+        public void GettingStarted()
+        {
+            // TestStack.White 0.13.3
+            // https://www.nuget.org/packages/TestStack.White/
+            // Install-Package TestStack.White -Version 0.13.3 Northwind.White
+            var application = Application.Launch(@"..\..\..\Northwind.UI\bin\Debug\Northwind.UI.exe");
+            var main = application.GetWindow("Northwind");
+            var mainMenu = main.Get<ListBox>();
+            mainMenu.Item("Employees").Select();
+            main.Get<Button>(SearchCriteria.ByText("Add")).Click();
+
+            var newEmployee = Retry.For(
+                () => application.GetWindows().First(x => x.Title.Contains("New employee")),
+                TimeSpan.FromSeconds(5));
+            newEmployee.Get<TextBox>(SearchCriteria.Indexed(0)).Text = "Vladimir";
+            newEmployee.Get<TextBox>(SearchCriteria.Indexed(1)).Text = "Khorikov";
+            newEmployee.Get<TextBox>(SearchCriteria.Indexed(2)).Text = "I'm a software developer";
+            newEmployee.Get<Button>(SearchCriteria.ByText("Change")).Click();
+
+            var department = Retry.For(
+                () => application.GetWindows().First(x => x.Title.Contains("Change department")),
+                TimeSpan.FromSeconds(5));
+            department.Get<ListBox>().Item("Test department").Select();
+            department.Get<Button>(SearchCriteria.ByText("OK")).Click();
+
+            newEmployee.Get<Button>(SearchCriteria.ByText("OK")).Click();
+
+            var employee = Retry.For(
+                () => application.GetWindows().First(x => x.Title.Contains("Employee")),
+                TimeSpan.FromSeconds(5));
+            employee.Get<Button>(SearchCriteria.ByText("OK")).Click();
+
+            var listView = main.Get<ListView>();
+            listView.Cell("First Name", 0).Click();
+            main.Get<Button>(SearchCriteria.ByText("Edit")).Click();
+
+            var editEmployee = Retry.For(
+                () => application.GetWindows().First(x => x.Title.Contains("Employee")),
+                TimeSpan.FromSeconds(5));
+            var textBox = editEmployee.Get<TextBox>(SearchCriteria.Indexed(0));
+            Assert.AreEqual("Vladimir2", textBox.Text);
+        }
     }
 }
